4728731608cfee0edf8d486be0d50c98deec0e18
John <john.hardberger@gmail.com>
2019-02-22 13:06:19 -0600

    starting to implement parallax

--- photo-carousel/src/Display/index.js
+++ photo-carousel/src/Display/index.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+
 class Display extends Component {
 	render(){
-		
+
 		const allPhotos = this.props.photos.map((photo, i) => {
-			return(
-				<div className='photo-wrapper' key={i}>
-					<img src={photo.url} alt={photo.location} className='photo' />
-					<h4>{photo.location}</h4>
-				</div>
-			)
-		})
+			const offset = 50;
+            const odd = i % 2;
+
+            const props = {
+                offsetXMin: -offset + '%',
+                offsetXMax: offset + '%',
+                offsetYMin: -offset * odd + '%',
+                offsetYMax: offset * odd + '%',
+                slowerScrollRate: !!odd,
+            };
+
+            return (
+                <Parallax key={i} className='file' {...props}>
+                	<div className='image-wrapper'>
+                    	<img 
+                    		alt={photo.name} 
+                    		src={photo.url} 
+                    		className='photo'
+                    	/>
+                    </div>
+                </Parallax>
+            );
+        })
 
 		return(
 			<div className='display'>
 				<h1>Display here, baby</h1>
 				<div className='all-photos'>
-					{allPhotos}
+					<ParallaxProvider>
+						{allPhotos}
+					</ParallaxProvider>
 				</div>
 			</div>
 		)

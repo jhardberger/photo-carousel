8369114d4728f8b2e8a6ce568d59044c9a5510fe
John <john.hardberger@gmail.com>
2019-03-06 14:23:19 -0600

    added one last datapoint to modal

--- photo-carousel/src/PhotoModal/index.js
+++ photo-carousel/src/PhotoModal/index.js
@@ -3,15 +3,18 @@ import { Button, Image, Label, Modal } from 'semantic-ui-react';
 
 const PhotoModal = (props) => {
 	if (props.currentDisplay){
+
 		const photoTags = props.currentDisplay.tags.map((tag, i) => {
 			return (<Label as='a' key={i}>{tag}</Label>)			
-		})
+		});
+
 		return(
 			<div className='display-modal'>
 				<Modal basic open={props.showPhoto}>
 					<div className='display-inner'>
 						<Image src={props.currentDisplay.url} onClick={props.hidePhoto} />
 						<div className='tags'>
+							<h1>{props.currentDisplay.location}</h1>
 							{photoTags}
 						</div>
 					</div>

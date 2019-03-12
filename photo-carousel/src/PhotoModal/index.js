4e03b03e87e26b562fb096d8eacd809e24e6f81b
John <john.hardberger@gmail.com>
2019-03-11 19:09:30 -0500

    setup fade for photo info in CSS, update image size from semantic react ui

--- photo-carousel/src/PhotoModal/index.js
+++ photo-carousel/src/PhotoModal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Fade }					from 'react-reveal';
+import { Fade }				from 'react-reveal';
 import { Image, Label, Modal } 	from 'semantic-ui-react';
 
 const PhotoModal = (props) => {
@@ -14,28 +14,31 @@ const PhotoModal = (props) => {
 			<div className='photo-modal'>
 				<Modal basic open={props.showPhoto} >
 					<div className='modal-inner' onClick={props.hidePhoto}>
-						<div 
-							className='hover'
-							onMouseOver={props.displayPhotoTraits}
-							onMouseLeave={props.hidePhotoTraits}
-						></div>
+							<div 
+								className='hover'
+								onMouseOver={props.displayPhotoTraits}
+								onMouseLeave={props.hidePhotoTraits}
+							>
+								<Fade clear when={props.currentDisplay.display} >
+									<div className='photo-info'>
+										<h1 className='location'>
+											{props.currentDisplay.location}
+										</h1>
+										<Fade right cascade>
+											<div className='tags'>
+												{photoTags}
+											</div>
+										</Fade>
+										(click photo to exit)
+									</div>
+								</Fade>
+							</div>
 						<div className='photo-frame'>
-							<img 
+							<Image 
+								size='massive'
 								src={props.currentDisplay.url} 
 								className='photo-display'
 							/>
-							<Fade right when={props.currentDisplay.display} >
-								<div className='photo-info'>
-									<h1 className='location'>
-										{props.currentDisplay.location}
-									</h1>
-									<Fade right cascade>
-										<div className='tags'>
-											{photoTags}
-										</div>
-									</Fade>
-								</div>
-							</Fade>
 						</div>
 					</div>
 				</Modal>

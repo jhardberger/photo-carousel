704e0745d92e134573dd1a4065cdf80f1c053ceb
John <john.hardberger@gmail.com>
2019-03-14 15:11:00 -0500

    basic layout for welcome screen

--- photo-carousel/src/Title/index.js
+++ photo-carousel/src/Title/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { Icon } 					from 'semantic-ui-react';
 
 const Title = (props) => {
 	return(
-		<div className='title'>
-			<h1>Photo Carousel</h1>
+		<div className='copy'>
+			<h1 className='title'>the carousel</h1>
+			<h2>film and digital photography by <a href='johnhardberger.com'>john hardberger</a></h2>
+			<h2 className='scroll'>scroll</h2>
+			<Icon size='large' name='chevron down' className='scroll-arrow' color='white'/>		
 		</div>
 	)
 }
